3dbab2425fd4ff77c4d3f1e0896e703457b106f1
galileuveloso <galileu.veloso@hotmail.com>
2021-07-30 23:54:36 -0300

    refatoramento da AudioService e metodos da api

--- Dominio/Interfaces/IAudioService.cs
+++ Dominio/Interfaces/IAudioService.cs
@@ -1,40 +1,19 @@
-﻿using Dominio.Commands;
+﻿using Dominio.Models;
 using Dominio.Responses;
-using Refit;
+using System.Collections.Generic;
 using System.IO;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace Dominio.Interfaces
 {
     public interface IAudioService
     {
         byte[] LerBytesAudio(Stream stream);
-        Task<AdicionarAudioResponse> AdicionarAudio(AdicionarAudioCommand command);
-        Task<AdicionarAudioYouTubeResponse> AdicionarAudioYouTube(AdicionarAudioYouTubeCommand command);
-        Task<ListarAudiosResponse> ListarAudios(int idUsuaio);
-        Task<ExcluirAudioResponse> ExcluirAudio(ExcluirAudioCommand command);
-        Task<EditarAudioResponse> EditarAudio(EditarAudioCommand command);
+        AdicionarAudioResponse AdicionarAudio(IList<AudioModel> audios);
+        AdicionarAudioYouTubeResponse AdicionarAudioYouTube(string titulo, string linkYouTube, int idUsuario);
+        ListarAudiosResponse ListarAudios(int idUsuaio);
+        ExcluirAudioResponse ExcluirAudio(int idAudio);
+        EditarAudioResponse EditarAudio(int idAudio, string novoTitulo);
     }
 
-    public interface IAudioServiceApi
-    {
-        [Post("/audio/reproduzir")]
-        Task<HttpContent> ReproduzirAudio([Body] ReproduzirAudioCommand command);
 
-        [Post("/audio/adicionar")]
-        Task<HttpContent> AdicionarAudio([Body] AdicionarAudioCommand command);
-
-        [Post("/audio/adicionaryt")]
-        Task<HttpContent> AdicionarAudioYouTube([Body] AdicionarAudioYouTubeCommand command);
-
-        [Delete("/audio/excluir")]
-        Task<HttpContent> ExcluirAudio([Body] ExcluirAudioCommand command);
-
-        [Patch("/audio/editar")]
-        Task<HttpContent> EditarAudio([Body] EditarAudioCommand command);
-
-        [Get("/audio/listar/{idUsuario}")]
-        Task<HttpResponseMessage> ListarAudios([AliasAs("idUsuario")] int idUsuario);
-    }
 }
